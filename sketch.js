d3c1a294239df65e895fb4eed44f99123e331c00
arafnokib <70985187+arafnokib@users.noreply.github.com>
2020-11-30 15:26:54 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+var cars, car1, car2, car3, car4;
+var c1, c2, c3, c4, grnd, track;
+
+function preload(){
+    c1 = loadImage("car1.png");
+    c2 = loadImage("car2.png");
+    c3 = loadImage("car3.png");
+    c4 = loadImage("car4.png");
+    grnd = loadImage("ground.png")
+    track = loadImage("track.jpg");
+
+}
+
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  
+    console.log(gameState)
+  
+  //console.log(mouseY)
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+  }
+}
